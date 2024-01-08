48158b405338ff5f681a35f7316692fd509c8e2c
Moiz Qureshi <155383697+themoizqureshi@users.noreply.github.com>
2024-01-08 11:17:57 +0530

    User Login and Logout implemented

---
+++ src/lib/supabase/server.tsx
@@ -0,0 +1,19 @@
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export default async function supabaseServer() {
+  const cookieStore = cookies();
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+      },
+    }
+  );
+  return supabase;
+}
