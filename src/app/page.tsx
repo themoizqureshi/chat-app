f352ec1b5c33b9d04e9cd7d8101106470b8df480
Moiz Qureshi <155383697+themoizqureshi@users.noreply.github.com>
2024-01-09 10:56:52 +0530

    initialized user state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import ChatHeader from "@/components/ChatHeader";
 import { Button } from "@/components/ui/button";
+import InitUser from "@/lib/store/initUser";
 import supabaseServer from "@/lib/supabase/server";
 import React from "react";
 
@@ -9,11 +10,14 @@ async function Page() {
 
   console.log(data);
   return (
-    <div className="max-w-3xl mx-auto md:py-10 h-screen">
-      <div className="h-full border rouded-md">
-        <ChatHeader user={data.session?.user} />
+    <>
+      <div className="max-w-3xl mx-auto md:py-10 h-screen">
+        <div className="h-full border rouded-md">
+          <ChatHeader user={data.session?.user} />
+        </div>
       </div>
-    </div>
+      <InitUser user={data.session?.user} />
+    </>
   );
 }
 
